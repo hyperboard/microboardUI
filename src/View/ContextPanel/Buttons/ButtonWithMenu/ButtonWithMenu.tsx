b68b804b9237bc932c3c50498b2d9326104853ed
Kirill Naumenko <naumenkokirill466@gmail.com>
2024-07-03 03:46:20 +0300

    fix: menu initial position calcs

--- src/View/ContextPanel/Buttons/ButtonWithMenu/ButtonWithMenu.tsx
+++ src/View/ContextPanel/Buttons/ButtonWithMenu/ButtonWithMenu.tsx
@@ -1,6 +1,12 @@
 import { Mbr } from "Board/Items";
 import clsx from "clsx";
-import React, { ReactNode, useEffect, useRef, useState } from "react";
+import React, {
+	ReactNode,
+	useEffect,
+	useLayoutEffect,
+	useRef,
+	useState,
+} from "react";
 import style from "./UiButtonWithMenu.module.css";
 
 type RenderNode = (verticalAlign: "middle" | "top" | "bottom") => ReactNode;
@@ -35,7 +41,8 @@ export function ButtonWithMenu({
 			return;
 		}
 		const menuHeight = menu.getBoundingClientRect().height;
-
+		console.log(panelMbr.bottom + menuHeight < windowHeight);
+		console.log(panelMbr.bottom, menuHeight, windowHeight);
 		if (panelMbr.bottom + menuHeight < windowHeight) {
 			setVerticalAlign("bottom");
 			return;
@@ -45,25 +52,26 @@ export function ButtonWithMenu({
 		// 	return;
 		// }
 		setVerticalAlign("middle");
-	}, [panelMbr.top, windowHeight]);
+	}, [panelMbr, windowHeight]);
 
 	return (
 		<div className={style.container}>
 			{typeof button === "function" ? button(verticalAlign) : button}
-			{openedMenu === menuName && (
-				<div
-					ref={menuRef}
-					className={clsx([
-						style.menu,
-						style[verticalAlign],
-						style[align],
-					])}
-				>
-					{typeof children === "function"
-						? children(verticalAlign)
-						: children}
-				</div>
-			)}
+			{/* {openedMenu === menuName && ( */}
+			<div
+				ref={menuRef}
+				className={clsx([
+					style.menu,
+					style[verticalAlign],
+					style[align],
+					style[openedMenu === menuName ? "opened" : "closed"],
+				])}
+			>
+				{typeof children === "function"
+					? children(verticalAlign)
+					: children}
+			</div>
+			{/* )} */}
 		</div>
 	);
 }
