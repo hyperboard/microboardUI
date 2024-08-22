ac772c6c409d8a7b2d6fa90dac9eefa3e85cfffb
Kirill Naumenko <naumenkokirill466@gmail.com>
2024-08-22 11:41:56 +0300

    fix: welcome board image

--- src/Board/Items/RichText/Editor/TextNode.ts
+++ src/Board/Items/RichText/Editor/TextNode.ts
@@ -16,7 +16,14 @@ export type TextStyle = typeof TextStyles[number];
 export type TextNode = {
 	type: "text";
 	text: string;
-	styles?: TextStyle[];
+	// styles?: TextStyle[];
+	bold: boolean;
+	italic: boolean;
+	underline: boolean;
+	overline: boolean;
+	lineThrough: boolean;
+	subscript: boolean;
+	superscript: boolean;
 	fontFamily?: string;
 	fontColor?: string;
 	fontSize?: number;
