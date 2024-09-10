71c5d72fb72b4facd502aca05294d7f4eb7a09d8
Oleksiy Oliinyk <aorniki@yandex.ru>
2024-09-10 14:11:21 +0300

    fix: not start job while exported no taskId

--- src/Routes/V1/Talk/Router.ts
+++ src/Routes/V1/Talk/Router.ts
@@ -6,21 +6,28 @@ export function createTalkRouter() {
 
     router.post(
         "/talk/transform",
-        body("taskId").isString(),
-        body("userId").isString().optional(),
+        body("taskId").isString().notEmpty(),
+        body("userId").optional().isString(),
         async (req: Request, res: Response) => {
             try {
+                const taskId = req?.body?.taskId;
+                if (!taskId) {
+                    return res.status(400).json({
+                        error: "Invalid task id",
+                        message: `Provided task id: ${taskId}`,
+                    });
+                }
                 const job = await talkIntegrationJob.invoke({
-                    id: req.body.taskId,
+                    id: taskId,
                     userId: req.body.userId || "0",
                 });
 
-                res.status(200).json({
+                return res.status(200).json({
                     message: "Talk integration job started",
                     job,
                 });
             } catch (e) {
-                res.status(500).json({
+                return res.status(500).json({
                     error: "Error to start talk integration job",
                 });
             }
