0a7b109d2636ed4f6da59da66a291238bea1a3e8
Ivan <antipinivan0491@gmail.com>
2024-09-23 13:42:57 +0300

    Удалил пустой файл и закоментил нерабочий тест c chai

--- src/Board/Items/Transformation/testTransformation.ts
+++ src/Board/Items/Transformation/testTransformation.ts
@@ -1,19 +1,19 @@
-import { assert } from "chai";
-import { Transformation } from "./Transformation";
+// import { assert } from "chai";
+// import { Transformation } from "./Transformation";
 
-export function testTransformation(transform: Transformation): void {
-	describe("to diagram, a user", () => {
-		it("Test transformation", () => {
-			transform.translateTo(100, 100);
-			transform.scaleBy(2, 2);
-			transform.rotateBy(360);
+// export function testTransformation(transform: Transformation): void {
+// 	describe("to diagram, a user", () => {
+// 		it("Test transformation", () => {
+// 			transform.translateTo(100, 100);
+// 			transform.scaleBy(2, 2);
+// 			transform.rotateBy(360);
 
-			assert.deepStrictEqual(transform.getTranslation(), {
-				x: 100,
-				y: 100,
-			});
-			assert.deepStrictEqual(transform.getScale(), { x: 2, y: 2 });
-			assert.strictEqual(transform.getRotation(), 0);
-		});
-	});
-}
+// 			assert.deepStrictEqual(transform.getTranslation(), {
+// 				x: 100,
+// 				y: 100,
+// 			});
+// 			assert.deepStrictEqual(transform.getScale(), { x: 2, y: 2 });
+// 			assert.strictEqual(transform.getRotation(), 0);
+// 		});
+// 	});
+// }
