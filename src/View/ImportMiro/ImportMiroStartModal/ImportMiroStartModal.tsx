50531aec36c61dafae7bb9f60d0eabf704ee0b46
Kate Plaksina <plaksina.larisa@yandex.ru>
2024-11-28 09:45:26 +0300

    feat(MB-189): add modal instructions for import miro v2

--- src/View/ImportMiro/ImportMiroStartModal/ImportMiroStartModal.tsx
+++ src/View/ImportMiro/ImportMiroStartModal/ImportMiroStartModal.tsx
@@ -1,31 +1,17 @@
 import { Modal } from "shared/ui-lib/Modal";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import ImportMiroStartImg from "shared/assets/imgs/importMiroStart.png";
-import { UiButton } from "View/Ui/UiButton";
 import styles from "./ImportMiroStartModal.module.css";
 import { useModal } from "View/Modal/ModalProvider";
+import { Button } from "shared/ui-lib/Button";
+import { Icon } from "View/Icon";
 
 export const ImportMiroStartModal = (): JSX.Element => {
 	const { t } = useTranslation();
 	const { isModalOpen, hideModal } = useModal();
 
-	// TODO fix loader png
-	const importMiroStartImg = ImportMiroStartImg?.toString().replace(".", "");
-
 	const onClick = (): void => {
-		// TODO using ENV after transferring to the client
-		// const clientId = import.meta.env.MIRO_CLIENT_ID;
-		// const clientSecret = import.meta.env.MIRO_CLIENT_SECRET;
-		// const baseURl = import.meta.env.BASE_URL
-		const clientId = "3458764589599848573";
-		const redirectUrl = window.location.origin + "/boards/blank";
-
-		window.location.href =
-			"https://miro.com/oauth/authorize?response_type=code&client_id=" +
-			clientId +
-			"&redirect_uri=" +
-			redirectUrl;
+		window.location.href = "https://miro.com/app/dashboard/";
 	};
 
 	return (
@@ -35,13 +21,30 @@ export const ImportMiroStartModal = (): JSX.Element => {
 			modalName="startImportMiro"
 		>
 			<h3 className={styles.title}>{t("miro.importMiro")}</h3>
-			<p className={styles.text}>{t("miro.startModalText")}</p>
-			<div className={styles.img}>
-				<img src={importMiroStartImg} alt={t("miro.importMiro")} />
+			<p className={styles.text}>{t("miro.startModal.text")}</p>
+			<div className={styles.btnsWr}>
+				<Button
+					pattern="tertiary"
+					onClick={() => hideModal("startImportMiro")}
+					className={styles.cancelBtn}
+				>
+					{t("miro.startModal.cancel")}
+				</Button>
+				<Button
+					id={"miro"}
+					pattern="primary"
+					onClick={onClick}
+					className={styles.goToMiroBtn}
+				>
+					<Icon
+						iconName="miro"
+						width={16}
+						height={16}
+						style={{ color: "#050038" }}
+					/>
+					{t("miro.startModal.goToMiroBtn")}
+				</Button>
 			</div>
-			<UiButton onClick={onClick} size="sm">
-				{t("miro.nextStepBtn")}
-			</UiButton>
 		</Modal>
 	);
 };
