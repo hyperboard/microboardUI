ba94249fa197174446b55f3bea119392e38213e0
susemaa <rad.archeg@gmail.com>
2025-01-20 16:36:28 +0700

    removed sepolia

--- src/View/ContextWrapper.tsx
+++ src/View/ContextWrapper.tsx
@@ -14,7 +14,7 @@ import { ToastProvider } from "./ToastProvider";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
-import { arbitrum, mainnet, polygon, sepolia } from "wagmi/chains";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
 import { AIContextProvider } from "./AIInput/AIContext";
 
 type Props = {
@@ -25,7 +25,7 @@ type Props = {
 const config = getDefaultConfig({
 	appName: "board_test",
 	projectId: "b1c6e6a21e23505e28fe385a0da4135f",
-	chains: [mainnet, polygon, arbitrum, sepolia],
+	chains: [mainnet, polygon, arbitrum],
 });
 const queryClient = new QueryClient();
 
