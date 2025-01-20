21c6aeec61fb3cc3d0c883a0a2737fbebaae37f8
Kirill Naumenko <naumenkokirill466@gmail.com>
2025-01-20 11:34:45 +0300

    fix model selection

--- src/View/AIInput/AIInput.tsx
+++ src/View/AIInput/AIInput.tsx
@@ -91,16 +91,16 @@ export const AIInput: React.FC = () => {
 		event.target.style.height = `${Math.min(event.target.scrollHeight, 120)}px`;
 	};
 
-	useEffect(() => {
-		if (!account.billingInfo?.models) {
-			return;
-		}
-		const defaultModel = account.billingInfo?.models.find(
-			model => model.isDefault && model.isEnabled,
-		);
+	// useEffect(() => {
+	// 	if (!account.billingInfo?.models) {
+	// 		return;
+	// 	}
+	// 	const defaultModel = account.billingInfo?.models.find(
+	// 		model => model.isDefault && model.isEnabled,
+	// 	);
 
-		setModel((defaultModel?.id as OpenAIModels) ?? "GPT4o mini");
-	}, [account.billingInfo?.models]);
+	// 	setModel((defaultModel?.id as OpenAIModels) ?? "GPT4o mini");
+	// }, [account.billingInfo?.models]);
 
 	const handleSendClick = async (ev: SyntheticEvent) => {
 		ev.stopPropagation();
