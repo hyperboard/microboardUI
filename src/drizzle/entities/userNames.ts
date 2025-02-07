aff19644587889135eaa023b175899916946064f
Kirill Naumenko <naumenkokirill466@gmail.com>
2025-02-07 10:43:27 +0300

    fix username change

--- src/drizzle/entities/userNames.ts
+++ src/drizzle/entities/userNames.ts
@@ -1,7 +1,9 @@
-import { pgTable, varchar, integer } from 'drizzle-orm/pg-core';
-import { users } from './users';
+import { pgTable, varchar, integer } from "drizzle-orm/pg-core";
+import { users } from "./users";
 
-export const userNames = pgTable('user_name', {
-  userId: integer('user_id').references(() => users.id, { onDelete: 'cascade' }),
-  name: varchar('name', { length: 100 }),
+export const userNames = pgTable("user_name", {
+    userId: integer("user_id")
+        .references(() => users.id, { onDelete: "cascade" })
+        .unique(),
+    name: varchar("name", { length: 100 }),
 });
---
+++ src/drizzle/migrations/0013_tan_joseph.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "user_name" ADD CONSTRAINT "user_name_user_id_unique" UNIQUE("user_id");
