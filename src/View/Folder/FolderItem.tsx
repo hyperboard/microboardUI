399aba8594a67d7a09113b6e85f1ec7fdbe99f44
Kate Plaksina <plaksina.larisa@yandex.ru>
2025-02-12 10:09:54 +0300

    upd: add zoomToFit when opening board from folders

--- src/View/Folder/FolderItem.tsx
+++ src/View/Folder/FolderItem.tsx
@@ -113,6 +113,9 @@ export const FolderItem = forwardRef<HTMLDivElement, Props>(
 					}
 					await app.openBoard(board.id);
 					navigate(`/boards/${board.id}`);
+					const newBoard = app.getBoard();
+					const itemsMbr = newBoard.items.getMbr();
+					newBoard.camera.zoomToFit(itemsMbr)
 				},
 				ev => {
 					ev.preventDefault();
