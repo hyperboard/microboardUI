ccfd1d80d9a94ea4634402002be2d98c0cd02c01
Kirill Naumenko <naumenkokirill466@gmail.com>
2025-02-17 10:26:53 +0300

    fix stripe subscription

--- src/Routes/V1/Billing/Router.ts
+++ src/Routes/V1/Billing/Router.ts
@@ -177,6 +177,7 @@ export const getBillingRouter = (
 
             if (!stripeCustomerId) {
                 stripeCustomerId = (await stripeService.createStripeCustomer(userToken.sub)).id;
+                await redis.client.set(`stripe:user:${userToken.sub}`, stripeCustomerId);
             }
 
             const session = await stripeService.createCheckoutSession({
