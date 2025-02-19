c53970ddbb9baf93cf86b2add8c5079f59a2f5e6
Kirill Naumenko <naumenkokirill466@gmail.com>
2025-02-19 14:09:16 +0300

    refactor createVectorExtension

--- src/drizzle/db/index.ts
+++ src/drizzle/db/index.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import { createVectorExtension } from "drizzle/scripts/create-vector-ext";
 
 dotenv.config();
 
--- src/drizzle/scripts/create-vector-ext.ts
+++ src/drizzle/scripts/create-vector-ext.ts
@@ -1,21 +1,19 @@
 import { Pool } from "pg";
 
 export async function createVectorExtension(pool: Pool) {
-    (async () => {
-        const client = await pool.connect();
-        try {
-            await client.query(`
+    const client = await pool.connect();
+    try {
+        await client.query(`
         DO $$ BEGIN
           IF NOT EXISTS (SELECT FROM pg_extension WHERE extname = 'vector') THEN
             CREATE EXTENSION IF NOT EXISTS "vector";
           END IF;
         END $$;
       `);
-            console.log("pg_vector extension is ensured.");
-        } catch (error) {
-            console.error("Error ensuring pg_vector extension:", error);
-        } finally {
-            client.release();
-        }
-    })();
+        console.log("pg_vector extension is ensured.");
+    } catch (error) {
+        console.error("Error ensuring pg_vector extension:", error);
+    } finally {
+        client.release();
+    }
 }
