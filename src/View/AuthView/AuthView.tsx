beb5ba3d5e4a36a58afaa21d34118c133b67aba0
Kirill Naumenko <naumenkokirill466@gmail.com>
2025-03-03 07:45:07 +0300

    fix google auth

--- src/View/AuthView/AuthView.tsx
+++ src/View/AuthView/AuthView.tsx
@@ -1,10 +1,31 @@
 import { Navbar } from "View/Widgets/Navbar/Navbar";
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import "./AuthView.css";
 import QuickAddButtonsClear from "Board/Selection/QuickAddButtons/QuickAddButtonsClear";
+import { notify } from "View/Ui/Toast";
+import { useTranslation } from "react-i18next";
 
 const AuthView: React.FC = () => {
+	const location = useLocation();
+	const navigate = useNavigate();
+	const { t } = useTranslation();
+
+	useEffect(() => {
+		const params = new URLSearchParams(location.search);
+		if (params.get("authException") === "true") {
+			setTimeout(() => {
+				notify({
+					header: t("auth.unknownError"),
+					body: t("auth.googleError"),
+					variant: "error",
+				});
+			}, 0);
+			params.delete("authException");
+			navigate({ search: params.toString() }, { replace: true });
+		}
+	}, [location, navigate]);
+
 	return (
 		<QuickAddButtonsClear>
 			<div className="AuthView">
