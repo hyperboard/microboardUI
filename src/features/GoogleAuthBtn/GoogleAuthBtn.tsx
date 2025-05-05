42d45c098c1bc65863cd295712c1ad81a222d541
Kirill Naumenko <naumenkokirill466@gmail.com>
2025-05-05 11:50:31 +0300

    update workflow

--- src/features/GoogleAuthBtn/GoogleAuthBtn.tsx
+++ src/features/GoogleAuthBtn/GoogleAuthBtn.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Icon } from "shared/ui-lib/Icon";
 import { useTranslation } from "react-i18next";
+import { Icon } from "shared/ui-lib/Icon";
 import styles from "./GoogleAuthBtn.module.css";
 
-export function GoogleAuthBtn(): JSX.Element {
+export function GoogleAuthBtn() {
 	const { t } = useTranslation();
 
 	const handleClick = (_ev): void => {
