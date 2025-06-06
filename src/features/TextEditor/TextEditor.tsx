5d3d54b9cee8bcee558490715612dfa3b0f85f69
Alexandr Panchenko <alexandr.panchenko.93@yandex.ru>
2025-06-06 10:58:02 +0300

    Revert "install microboard"
    This reverts commit 73df40ada65512a2a4d1d9541e4f345b49b9817b.

--- src/features/TextEditor/TextEditor.tsx
+++ src/features/TextEditor/TextEditor.tsx
@@ -3,7 +3,7 @@ import { Slate, Editable } from "slate-react";
 import { Leaf } from "./Leaf";
 import { Element } from "./Element";
 import { App } from "App";
-import { Board } from "microboard-temp";
+import { Board } from "Board";
 import { verticalAlignmentToFlex } from "./verticalAlignmentToFlex";
 import { RichText } from "Board/Items/RichText/RichText";
 import styles from "./TextEditor.module.css";
@@ -502,7 +502,7 @@ export class TextEditor extends React.Component<
 					>
 						<Slate
 							editor={text.editor.editor}
-							initialValue={text.getText()}
+							value={text.getText()}
 							// key={text.getId()}
 							key={text.rtCounter}
 							onChange={this.handleSelectionChange}
