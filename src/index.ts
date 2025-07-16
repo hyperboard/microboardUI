e9041adbd4b8478714ade91152dae5d96366e073
Arsenii <rad.archeg@gmail.com>
2025-07-16 19:56:34 +0700

    upd exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { App, createApp } from "App/App";
 export { initInter } from "initI18N";
 export { customCursors } from "features/Cursors";
+export { default as sprite } from "./shared/ui-lib/Icon/sprite.svg";
 import "./index.css";
