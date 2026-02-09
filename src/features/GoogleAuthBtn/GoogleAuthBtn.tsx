c2c11c89b5444afb77d80e3b9de075b09a3f208c
AndreyKa <andreyka.kameshkov337@mail.ru>
2026-02-09 16:58:26 +0300

    fix: google auth

--- src/features/GoogleAuthBtn/GoogleAuthBtn.tsx
+++ src/features/GoogleAuthBtn/GoogleAuthBtn.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { Icon } from "shared/ui-lib/Icon";
 import styles from "./GoogleAuthBtn.module.css";
+import { getApiUrl } from "Config";
 
 export function GoogleAuthBtn() {
   const { t } = useTranslation();
 
   const handleClick = (_ev): void => {
-    window.location.replace(`${window.location.origin}/api/v1/auth/google`);
+    window.location.replace(getApiUrl("auth/google"));
   };
 
   return (
