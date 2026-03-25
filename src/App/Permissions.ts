f8d72513ba63bb6e21ffd529df66a5a9f2173816
AndreyKa <andreyka.kameshkov337@mail.ru>
2026-03-25 14:31:01 +0300

    fix: account sircular

--- src/App/Permissions.ts
+++ src/App/Permissions.ts
@@ -10,10 +10,6 @@ export class Permissions {
     private readonly storage: Storage,
   ) {}
 
-  toJSON(): undefined {
-    return undefined;
-  }
-
   checkPermissions(
     action: Action,
     resource: Resource,
